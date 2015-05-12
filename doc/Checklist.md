1a90b647e9af9b682223b7132b3094cc7e3281de
Rick Haffey <rhaffey@fanatics.com>
2015-05-11 19:52:17 -0500

    initial posts and src

---
+++ doc/Checklist.md
@@ -0,0 +1,10 @@
+# Blog Posting Checklist
+
+
+* [ ] Add and verify links back to previous post
+* [ ] Update previous post with forward link to 'next'
+* [ ] Run all code to verify that it works
+* [ ] Correct Categories
+* [ ] Correct Tags
+
+
---
+++ src/dot_net/textual-development.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textual_development.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
