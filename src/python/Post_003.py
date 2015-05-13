44563ccf9f971253427262a27583a27d67489faa
Rick Haffey <rhaffey@fanatics.com>
2015-05-13 14:44:01 -0500

    update post 003 python with Requests transport

--- src/python/Post_003.py
+++ src/python/Post_003.py
@@ -1,7 +1,26 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 import json
 
-host = "localhost"
-es = Elasticsearch("http://" + host + ":9200")
 
-print(json.dumps(es.info(), indent=4, sort_keys=True))
+def specify_host_connect(host):
+    # simple connection
+    return Elasticsearch(host=host, port=9200)
+
+def specify_host_proxy_connect(host):
+    # simple connection
+    # with proxy support so we can capture traffic via Fiddler
+    return Elasticsearch(host=host, port=9200, connection_class=RequestsHttpConnection)
+
+def connection_pool_connect(host):
+    # asks for nodes in the cluster,
+    # then calls nodes round-robin, 
+    # and provides failover support
+    return Elasticsearch(host=host, port=9200)
+
+def get_info(client):
+    return json.dumps(es.info(), indent=4, sort_keys=True)
+
+
+if __name__ == "__main__":
+    es = specify_host_proxy_connect('localhost')
+    print(get_info(es))
